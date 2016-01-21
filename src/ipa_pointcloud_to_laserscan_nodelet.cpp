9c18a7becebc217fe2ff104b6453f88bf052779b
svn <sofie.nilsson@ipa.fraunhofer.de>
2016-01-21 15:39:38 +0100

    Reduce computational time by  - determining which points out of the pointcloud to use in the pointcloud source frame instead of in the target frame (the borders are transformed to the pointcloud frame instead of other way around). - calculate the scan point out of the original point cloud.

--- src/ipa_pointcloud_to_laserscan_nodelet.cpp
+++ src/ipa_pointcloud_to_laserscan_nodelet.cpp
@@ -2,6 +2,7 @@
  * Software License Agreement (BSD License)
  *
  *  Copyright (c) 2010-2012, Willow Garage, Inc.
+ *  Copyright (c) 2015, Fraunhofer IPA.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -14,7 +15,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *   * Neither the name of the copyright holders nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -36,6 +37,7 @@
 
 /*
  * Author: Paul Bovbel
+ * Author: Sofie Nilsson
  */
 
 #include <pointcloud_to_laserscan/pointcloud_to_laserscan_nodelet.h>
@@ -43,6 +45,7 @@
 #include <pluginlib/class_list_macros.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 namespace pointcloud_to_laserscan
 {
@@ -150,6 +153,55 @@ namespace pointcloud_to_laserscan
     }
     // End hack for tf2 compatibility
 
+    // Get frame tranformation
+    tf2::Transform T;
+
+    if ((!target_frame_.empty()) && !(target_frame_ == cloud_msg->header.frame_id))
+    {
+      try
+      {
+            geometry_msgs::TransformStamped T_geom = tf2_->lookupTransform(cloud_msg->header.frame_id, target_frame_, ros::Time(0));
+            // Convert geometry msgs transform to tf2 transform.
+            tf2::fromMsg(T_geom.transform, T);
+      }
+      catch (tf2::TransformException ex)
+      {
+        NODELET_ERROR_STREAM("Transform failure: " << ex.what());
+        return;
+      }
+    }
+    else
+    {
+        // target and source frame are the same
+        T.setIdentity();
+    }
+
+    // Transform borders and target plane to original coordinates (saved resources to not have to transform the whole point cloud)
+    // A plane is described by all points fulfilling p= A + l1*e1 + l2*e2.
+    // Transformation to other coordinate frame with transformation T gives: p'= T(A) + l1*T(e1) + l2*T(e2)
+    // It is here assumed that both the border planes and target plane are paralell to each other -> same e1 and e2
+    // Planes for max and min height: A = [0; 0; max_height]
+    tf2::Vector3 A_max_t_frame(0, 0, max_height_);
+    tf2::Vector3 A_max_o_frame = T(A_max_t_frame);
+    NODELET_DEBUG_STREAM("A max: " << A_max_o_frame.getX() <<", "<< A_max_o_frame.getY() <<", "<< A_max_o_frame.getZ());
+
+    tf2::Vector3 A_min_t_frame(0, 0, min_height_);
+    tf2::Vector3 A_min_o_frame = T(A_min_t_frame); // want to get the orientation vector in the new coordinates
+    NODELET_DEBUG_STREAM("A min: " << A_min_o_frame.getX() <<", "<< A_min_o_frame.getY() <<", "<< A_min_o_frame.getZ());
+
+    // Transform plane basis vectors -> use only rotation
+    tf2::Vector3 ex_t_frame(1, 0, 0);
+    tf2::Vector3 ex_o_frame = tf2::quatRotate(T.getRotation(), ex_t_frame);
+    NODELET_DEBUG_STREAM("ex min: " << ex_o_frame.getX() <<", "<< ex_o_frame.getY() <<", "<< ex_o_frame.getZ());
+
+    tf2::Vector3 ey_t_frame(0, 1, 0);
+    tf2::Vector3 ey_o_frame = tf2::quatRotate(T.getRotation(), ey_t_frame);
+    NODELET_DEBUG_STREAM("ey min: " << ey_o_frame.getX() <<", "<< ey_o_frame.getY() <<", "<< ey_o_frame.getZ());
+
+    // Plane for target scan:
+    tf2::Vector3 A_target_t_frame(0, 0, 0);
+    tf2::Vector3 A_target_o_frame = T(A_target_t_frame);
+
     //build laserscan output
     sensor_msgs::LaserScan output;
     output.header = cloud_msg->header;
@@ -179,74 +231,81 @@ namespace pointcloud_to_laserscan
       output.ranges.assign(ranges_size, output.range_max + 1.0);
     }
 
-    sensor_msgs::PointCloud2ConstPtr cloud_out;
-    sensor_msgs::PointCloud2Ptr cloud;
-
-    // Transform cloud if necessary
-    if (!(output.header.frame_id == cloud_msg->header.frame_id))
-    {
-      try
-      {
-        cloud.reset(new sensor_msgs::PointCloud2);
-        tf2_->transform(*cloud_msg, *cloud, target_frame_, ros::Duration(tolerance_));
-        cloud_out = cloud;
-      }
-      catch (tf2::TransformException ex)
-      {
-        NODELET_ERROR_STREAM("Transform failure: " << ex.what());
-        return;
-      }
-    }
-    else
-    {
-      cloud_out = cloud_msg;
-    }
-
     // Iterate through pointcloud
-    for (sensor_msgs::PointCloud2ConstIterator<float>
-              iter_x(*cloud_out, "x"), iter_y(*cloud_out, "y"), iter_z(*cloud_out, "z");
+    for (sensor_msgs::PointCloud2Iterator<float>
+              iter_x(*cloud, "x"), iter_y(*cloud, "y"), iter_z(*cloud, "z");
               iter_x != iter_x.end();
               ++iter_x, ++iter_y, ++iter_z)
     {
+        if (std::isnan(*iter_x) || std::isnan(*iter_y) || std::isnan(*iter_z))
+        {
+            NODELET_DEBUG("rejected for nan in point(%f, %f, %f)\n", *iter_x, *iter_y, *iter_z);
+            continue;
+        }
 
-      if (std::isnan(*iter_x) || std::isnan(*iter_y) || std::isnan(*iter_z))
-      {
-        NODELET_DEBUG("rejected for nan in point(%f, %f, %f)\n", *iter_x, *iter_y, *iter_z);
-        continue;
-      }
+        //get reflection point in hight limiting planes in order to check that point lies between borders(above or below is not clearly def):
+        tf2::Vector3 P(*iter_x, *iter_y, *iter_z);
+        /**
+         * lambda x and y describes the location within the planes, which are the same for all paralell planes with
+         * aligned origin -> calculate only once for the plane at height of target frame.
+         * If assumption not hold, use one set of lambda per plane (use the A value of that specific plane like this:
+         *
+         * double lambda_x_max =  (P - A_max_o_frame).dot(ex_o_frame);
+         *
+         * For now we assume tahat a common set of lambdas hold:
+         */
+        double lambda_x =  (P - A_target_o_frame).dot(ex_o_frame);
+        double lambda_y =  (P - A_target_o_frame).dot(ey_o_frame);
+        tf2::Vector3 P_max = A_max_o_frame + lambda_x*ex_o_frame + lambda_y*ey_o_frame;
+        tf2::Vector3 P_min = A_min_o_frame + lambda_x*ex_o_frame + lambda_y*ey_o_frame;
 
-      if (*iter_z > max_height_ || *iter_z < min_height_)
-      {
-        NODELET_DEBUG("rejected for height %f not in range (%f, %f)\n", *iter_z, min_height_, max_height_);
-        continue;
-      }
 
-      double range = hypot(*iter_x, *iter_y);
-      if (range < range_min_)
-      {
-        NODELET_DEBUG("rejected for range %f below minimum value %f. Point: (%f, %f, %f)", range, range_min_, *iter_x, *iter_y,
-                      *iter_z);
-        continue;
-      }
+        double border_distance_sqared = P_max.distance2(P_min);
 
-      double angle = atan2(*iter_y, *iter_x);
-      if (angle < output.angle_min || angle > output.angle_max)
-      {
-        NODELET_DEBUG("rejected for angle %f not in range (%f, %f)\n", angle, output.angle_min, output.angle_max);
-        continue;
-      }
+        if ((P.distance2(P_max) > border_distance_sqared) || (P.distance2(P_min) > border_distance_sqared)) // Originaly: (*iter_z > max_height_ || *iter_z < min_height_)
+        {
+            NODELET_DEBUG("rejected for height %f not in range (%f, %f)\n", *iter_z, min_height_, max_height_);
+            continue;
+        }
 
-      //overwrite range at laserscan ray if new range is smaller
-      int index = (angle - output.angle_min) / output.angle_increment;
-      if (range < output.ranges[index])
-      {
-        output.ranges[index] = range;
-      }
+        double range = hypot(lambda_x, lambda_y);
+
+
+        if (range < range_min_)
+        {
+            NODELET_DEBUG("rejected for range %f below minimum value %f. Point: (%f, %f, %f)", range, range_min_, *iter_x, *iter_y,
+             *iter_z);
+            continue;
+        }
+
+        double angle = atan2(lambda_y, lambda_x);
+        if (angle < output.angle_min || angle > output.angle_max)
+        {
+            NODELET_DEBUG("rejected for angle %f not in range (%f, %f)\n", angle, output.angle_min, output.angle_max);
+            continue;
+        }
+
+        //overwrite range at laserscan ray if new range is smaller
+        int index = (angle - output.angle_min) / output.angle_increment;
+        if (range < output.ranges[index])
+        {
+            output.ranges[index] = range;
+        }
 
     }
+
     pub_.publish(output);
   }
 
+      {
+      }
+
+      {
+      }
+
+      {
+      }
+  }
 }
 
 PLUGINLIB_DECLARE_CLASS(pointcloud_to_laserscan, PointCloudToLaserScanNodelet, pointcloud_to_laserscan::PointCloudToLaserScanNodelet, nodelet::Nodelet);
